d395479e5a087f0e16378c697b8929db1f8abf34
Noemi Losada <noemi.losada.estrella@elsevier.com>
2019-03-11 14:02:46 +0000

    Move secret to script folder

--- scripts/deploy-secret.sh
+++ scripts/deploy-secret.sh
@@ -3,9 +3,9 @@
 echo Provide keystore file name
 read KEY_STORE_NAME
 
-echo Provide desired kubernetes secrets-object name
+echo Provide desired kubernetes secret-object name
 read K8S_SECRETS_NAME
 
-kubectl delete secret ${K8S_SECRETS_NAME}
+kubectl delete secret ${K8S_SECRETS_NAME} > /dev/null 2>&1
 kubectl create secret generic ${K8S_SECRETS_NAME} --from-file=${KEY_STORE_NAME}
 
